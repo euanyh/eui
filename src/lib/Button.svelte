a38a78ec71d2e17f20d45c17e06597c8ccaa61b4
Yuhao <yuhao@Yuhaos-MacBook-Pro.local>
2022-01-13 22:07:17 +0800

    + button & grid

---
+++ src/lib/Button.svelte
@@ -0,0 +1,33 @@
+
+<div class="row">
+    <div class="col col-6">
+        <div class="btn btn-primary">hello</div>
+    </div>
+    <div class="col col-6">
+        <div class="btn btn-secondary">hello</div>
+    </div>
+    <div class="col col-1">
+        <div class="btn btn-success">hello</div>
+    </div>
+    <div class="col col-1">
+        <div class="btn btn-info">hello</div>
+    </div>
+    <div class="col col-1">
+        <div class="btn btn-warning">hello</div>
+    </div>
+    <div class="col col-1">
+        <div class="btn btn-danger">hello</div>
+    </div>
+    <div class="col col-6">
+        <div class="btn btn-link">hello</div>
+    </div>
+    <div class="col col-6">
+        <div class="btn btn-dark">hello</div>
+    </div>
+
+</div>
+
+<div class="d-flex justify-content-around">
+    <div class="btn btn-link">hello</div>
+    <div class="btn btn-dark">hello</div>
+</div>
